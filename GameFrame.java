fc4027a7289cd5729bbd77f889ae5bd32631c725
TJPhoenix64 <Tyler.jordan@gcsaz.org>
2025-10-03 15:33:46 -0700

    Create GameFrame.java

---
+++ GameFrame.java
@@ -0,0 +1,21 @@
+
+import java.awt.Color;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    public GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Platformer");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}
