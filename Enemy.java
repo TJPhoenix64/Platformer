270fb962a8dd3b3f0b608b5cb7791638ce38412c
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-10-15 18:39:24 -0700

    add Enemy class to handle enemy behavior and rendering

---
+++ Enemy.java
@@ -0,0 +1,33 @@
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Enemy {
+    BufferedImage image;
+    int row;
+    int col;
+    int width;
+    int height;
+
+    public Enemy(int row, int col, int width, int height) {
+        this.row = row;
+        this.col = col;
+        this.width = width;
+        this.height = height;
+        try {
+            image = ImageIO.read(new File("photos/redImage.jpg"));
+
+        } catch (IOException e) {
+        }
+    }
+
+    public void draw(Graphics g) {
+        if (image != null) {
+            g.drawImage(image, this.col * GamePanel.tileSize, this.row * GamePanel.tileSize, this.width, this.height,
+                    null);
+        }
+    }
+}
