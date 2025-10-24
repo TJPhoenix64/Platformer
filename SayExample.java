daabd255982e98548a00531e493cc6f1b2d1ca61
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-10-23 17:42:51 -0700

    added SayExample.java allows me to say things, can easily adapt to do any terminal command

---
+++ SayExample.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class SayExample {
+
+    public static void sayPhrase(String word) {
+        try {
+            // Use bash to run the macOS "say" command
+            ProcessBuilder builder = new ProcessBuilder("bash", "-c", "say " + word);
+            builder.start(); // no need to read output; "say" speaks it aloud
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
