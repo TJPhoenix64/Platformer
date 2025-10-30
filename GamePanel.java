582c08f3c12831e3764bc75d3f7fa66cb112c98c
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-10-30 07:56:21 -0700

    overhaul/bug fix mouse dragged method

--- GamePanel.java
+++ GamePanel.java
@@ -569,15 +569,37 @@ public final class GamePanel extends JPanel implements Runnable {
 
         @Override
         public void mouseDragged(MouseEvent e) {
+            if (state != GameState.EDITING) {
+                return;
+            }
+            System.out.println("Dragged");
             int x = e.getPoint().x;
             int y = e.getPoint().y;
             int row = y / TILE_SIZE;
             int col = x / TILE_SIZE;
-            if (editingLevel.contains(new Tile(row, col, true))) {
-                editingLevel.remove(new Tile(row, col, true));
+
+            // System.out.println("col: " + col + " row: " + row);
+            System.out.println("Obj: " + editingLevel.get(col, row));
+            if (editingLevel.containsTemp(col, row)) {
+                editingLevel.remove(col, row);
             }
-            if (!editingLevel.contains(new Tile(row, col, false))) {
-                editingLevel.addTile(new Tile(row, col, false));
+            if (!editingLevel.contains(col, row)) {
+                System.out.println("new grid");
+                if (null != type) {
+                    switch (type) {
+                        case TILES:
+                            editingLevel.addObject(new Tile(col, row, false));
+                            break;
+                        case SPIKES:
+                            editingLevel.addObject(new Spike(col, row, tempSpikeRotation, false));
+                            break;
+                        case CHECKPOINTS:
+                            editingLevel.addObject(new Checkpoint(col, row, false));
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         }
     }
