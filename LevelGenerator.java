c514b11e7a64f697c8f10c54272fd76947ca2ad1
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-10-29 17:18:19 -0700

    overhaul: added spike and checkpoint functionality, added safety too

--- LevelGenerator.java
+++ LevelGenerator.java
@@ -1,29 +1,72 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LevelGenerator {
-    static Level level = new Level();
 
     public Level getContentsOfFile(String fileName) {
+        Level level = new Level();
+
         Scanner sc;
         try {
             sc = new Scanner(new File(fileName));
-            String line = sc.nextLine();
-            String[] sets = line.split("_");
-            int x;
-            int y;
-            for (String elem : sets) {
-                elem = elem.substring(1, elem.length() - 1);
-                String[] items = elem.split(", ");
-                x = Integer.parseInt(items[0]);
-                y = Integer.parseInt(items[1]);
-                Tile tile = new Tile(x, y, false);
-                level.addTile(tile);
+            int row;
+            int col;
+            int rotation;
+            if (sc.hasNextLine()) {
+                String firstLine = sc.nextLine();
+                // System.out.println("got tiles");
+                if (firstLine.length() > 7) {
+                    firstLine = firstLine.substring(6);
+                    // System.out.println(firstLine);
+                    String[] tiles = firstLine.split("_");
+                    for (String tile : tiles) {
+                        String[] items = tile.split(",");
+                        col = Integer.parseInt(items[0]);
+                        row = Integer.parseInt(items[1]);
+                        Tile t = new Tile(col, row, false);
+                        level.addObject(t);
+                    }
+                }
             }
-        } catch (FileNotFoundException e) {
+
+            if (sc.hasNextLine()) {
+                String secondLine = sc.nextLine();
+                // System.out.println("got spikes");
+                if (secondLine.length() > 8) {
+                    secondLine = secondLine.substring(7);
+                    // System.out.println(secondLine);
+                    String[] spikes = secondLine.split("_");
+
+                    for (String spike : spikes) {
+                        String[] items = spike.split(",");
+                        col = Integer.parseInt(items[0]);
+                        row = Integer.parseInt(items[1]);
+                        rotation = Integer.parseInt(items[2]);
+                        Spike s = new Spike(col, row, rotation);
+                        level.addObject(s);
+                    }
+                }
+            }
+            if (sc.hasNextLine()) {
+                String thirdLine = sc.nextLine();
+                // System.out.println("Got checkpoints");
+                if (thirdLine.length() > 13) {
+                    thirdLine = thirdLine.substring(12);
+                    // System.out.println(thirdLine);
+                    String[] checkpoints = thirdLine.split("_");
+                    for (String checkpoint : checkpoints) {
+                        String[] items = checkpoint.split(",");
+                        col = Integer.parseInt(items[0]);
+                        row = Integer.parseInt(items[1]);
+                        Checkpoint c = new Checkpoint(col, row);
+                        level.addObject(c);
+                    }
+                }
+            }
+        } catch (Exception e) {
             System.out.println("Oh No! something went wrong with the file");
         }
+        // System.out.println("This is being run");
         return level;
     }
 
